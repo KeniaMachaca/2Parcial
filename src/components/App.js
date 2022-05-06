5d37dfd99b0d029d65ac45bc49f18c09e2733ab6
Kenia Machaca <kenia.my77@gmail.com>
2022-05-06 09:43:55 -0400

    primer componente

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Primer } from './Primer';
+import '../styles/App.css';
+
+
+function App() {
+  return (
+    <section className='card'>
+      <Primer/>
+    </section>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Primer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Primer() {
+  return (
+    <div className='primer-contenedor'>
+        <div>Description of what is done here</div>
+        <div className='primer-contenedor__pie'>
+            <span>Last Updated</span><span>3/8/2020</span>
+        </div>
+    </div>
+  )
+}
+
+export {Primer}
