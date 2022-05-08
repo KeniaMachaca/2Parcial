003a96a19bdde7a8ef047cbf181da963ee086cfa
Kenia Machaca <kenia.my77@gmail.com>
2022-05-08 09:46:45 -0400

    segundo componente

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Primer } from './Primer';
+import { Segundo } from './Segundo';
 import '../styles/App.css';
 
 
 function App() {
   return (
     <section className='card'>
+      <Segundo/>
       <Primer/>
     </section>
     
---
+++ src/components/Segundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Primer } from './Primer'
+
+function Segundo() {
+  return (
+    <div className=' segundo-container'>
+        <span>Agenda/Topic</span><span>SAAS-0000</span>
+    </div>
+  )
+}
+
+export { Segundo}
